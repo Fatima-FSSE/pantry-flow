712ee532271dc336dafc7bdaaea1ab2636a1288a
Fatima Riaz <fatima_gatech@outlook.com>
2024-08-02 22:48:56 -0500

    fixing firebase path

--- src/app/pantry/[uid]/page.js
+++ src/app/pantry/[uid]/page.js
@@ -1,5 +1,5 @@
 'use client';
-import { firestore } from "../firebase";
+import { firestore } from "../../firebase";
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import IconButton from '@mui/material/IconButton';
